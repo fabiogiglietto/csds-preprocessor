4d074995cfd3d469e64b8aaed81f8bc58da2d2ba
Fabio Giglietto <fabio.giglietto@gmail.com>
2025-03-29 15:20:32 +0000

    More details on YouTube data source

--- src/App.tsx
+++ src/App.tsx
@@ -652,7 +652,7 @@ function App() {
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 inline-block mr-1 mb-0.5" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
                   </svg>
-                  For YouTube: Upload a CSV file from YouTube Data Tools with columns 'videoId', 'channelTitle', 'channelId', 'publishedAt', and your selected object_id source
+                  For YouTube: Upload a CSV file from YouTube Data Tools "Video List" function (<a href="https://ytdt.digitalmethods.net/mod_videos_list.php" target="_blank" rel="noopener noreferrer" className="underline">https://ytdt.digitalmethods.net/mod_videos_list.php</a>) with columns 'videoId', 'channelTitle', 'channelId', 'publishedAt', and your selected object_id source
                 </p>
               )}
             </div>
